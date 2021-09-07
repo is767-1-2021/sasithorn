f8446ffcdae70e61312be6e80e4d522041ae7ca9
Sasithorn Rangseethumpunya <sasithorn-ran63@tbs.tu.ac.th>
2021-09-07 21:23:06 +0700

    Form with onSaved + Launcher Icon

---
+++ first_app/lib/Pages/fifth_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class FifthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Grid View'),
+      ),
+      body: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(7, (index) {
+            return InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/${index + 1}');
+              },
+              child: Container(
+                margin: EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).accentColor,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Center(
+                  child: Text(
+                    'Item ${index + 1}',
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                ),
+              ),
+            );
+          })),
+    );
+  }
+}
